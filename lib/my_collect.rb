8b4d04b8557d34809144d489b7438582f630838a
jpkim921 <jpkim921@gmail.com>
2018-01-17 03:08:10 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
+def my_collect(array)
+    i = 0
+    first_names = []
+    while i < array.length
+      yield(array[i])
+    end
+    first_names
+end
 
-
+my_collect(array){|name| name.split(" ").first}
