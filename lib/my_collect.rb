9e1aecabe309537f5eca5b8f0477c3a7a544e5cd
jpkim921 <jpkim921@gmail.com>
2018-01-17 03:12:43 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
 def my_collect(array)
     i = 0
-    first_names = []
+    # first_names = []
     while i < array.length
-      first_names << yield(array[i])
+      yield(array[i])
       i+=1
     end
-    first_names
+    
 end
 
 my_collect(array){|name| name.split(" ").first}
