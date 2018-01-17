de9ca8fda56c557dc87429be6fda188b97bb40a8
jpkim921 <jpkim921@gmail.com>
2018-01-17 03:47:08 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,9 @@
-# require 'pry'
+require 'pry'
 
 def my_collect(array)
     i = 0
     first_names = []
+    binding.pry
     while i < array.length
       first_names << yield(array[i])
       i+=1
